c8935229f3a51982f2a0625d19821ff662c726d8
Carlos Felipe <dutra4944@gmail.com>
2026-08-04 21:45:34 -0300

    Projeto inicial Spring Boot

---
+++ src/main/java/com/cd/Funcionario/models/FuncionarioModel.java
@@ -0,0 +1,43 @@
+package com.cd.Funcionario.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class FuncionarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cpf;
+
+    public FuncionarioModel(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ src/test/java/com/cd/Funcionario/FuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.Funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
